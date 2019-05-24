8831089aeaef4e122a589c7ce6381abf0dbb7e81
coolsquid <9624459+coolsquid@users.noreply.github.com>
2019-05-24 17:21:25 +0200

    The HUD ZenClass can now uncancel the food HUD render event.

--- src/main/java/coolsquid/hungertweaker/ClientEventHandler.java
+++ src/main/java/coolsquid/hungertweaker/ClientEventHandler.java
@@ -1,15 +1,18 @@
 package coolsquid.hungertweaker;
 
+import coolsquid.hungertweaker.ct.CTHUD;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
+import net.minecraftforge.fml.common.eventhandler.Event.Result;
+import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 public class ClientEventHandler {
 
-	@SubscribeEvent
+	@SubscribeEvent(priority = EventPriority.LOWEST, receiveCanceled = true)
 	public static void onRenderOverlay(RenderGameOverlayEvent.Pre event) {
 		if (event.getType() == ElementType.FOOD) {
-			event.setCanceled(true);
+			event.setCanceled(CTHUD.status == Result.DENY);
 		}
 	}
 }
--- src/main/java/coolsquid/hungertweaker/ct/CTHUD.java
+++ src/main/java/coolsquid/hungertweaker/ct/CTHUD.java
@@ -3,6 +3,7 @@ package coolsquid.hungertweaker.ct;
 import coolsquid.hungertweaker.ClientEventHandler;
 import crafttweaker.annotations.ZenRegister;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.eventhandler.Event.Result;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
 
@@ -10,13 +11,18 @@ import stanhebben.zenscript.annotations.ZenMethod;
 @ZenClass("mods.hungertweaker.HUD")
 public class CTHUD {
 
-	@ZenMethod
-	public static void enable() {
-		MinecraftForge.EVENT_BUS.unregister(ClientEventHandler.class);
-	}
+	public static Result status = Result.DEFAULT;
 
 	@ZenMethod
-	public static void disable() {
-		MinecraftForge.EVENT_BUS.register(ClientEventHandler.class);
+	public static void setStatus(int v) {
+		Result newStatus = Result.values()[v];
+		if (newStatus != status) {
+			if (status == Result.DEFAULT) {
+				MinecraftForge.EVENT_BUS.unregister(ClientEventHandler.class);
+			} else {
+				MinecraftForge.EVENT_BUS.register(ClientEventHandler.class);
+			}
+		}
+		status = newStatus;
 	}
 }
