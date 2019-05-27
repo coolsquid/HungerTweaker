a3b800fdfa7d9b1827b59fa24eda8207f55c330c
coolsquid <9624459+coolsquid@users.noreply.github.com>
2019-05-27 13:05:18 +0200

    Server-proofed the HUD ZenClass.

--- src/main/java/coolsquid/hungertweaker/HUDEventHandler.java
+++ src/main/java/coolsquid/hungertweaker/HUDEventHandler.java
@@ -4,11 +4,15 @@ import coolsquid.hungertweaker.ct.CTHUD;
 import net.minecraftforge.client.event.RenderGameOverlayEvent;
 import net.minecraftforge.client.event.RenderGameOverlayEvent.ElementType;
 import net.minecraftforge.fml.common.eventhandler.Event.Result;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
-public class ClientEventHandler {
+@SideOnly(Side.CLIENT)
+public class HUDEventHandler {
 
+	@SideOnly(Side.CLIENT)
 	@SubscribeEvent(priority = EventPriority.LOWEST, receiveCanceled = true)
 	public static void onRenderOverlay(RenderGameOverlayEvent.Pre event) {
 		if (event.getType() == ElementType.FOOD) {
--- src/main/java/coolsquid/hungertweaker/ct/CTHUD.java
+++ src/main/java/coolsquid/hungertweaker/ct/CTHUD.java
@@ -1,11 +1,13 @@
 package coolsquid.hungertweaker.ct;
 
-import coolsquid.hungertweaker.ClientEventHandler;
+import coolsquid.hungertweaker.HUDEventHandler;
 import coolsquid.hungertweaker.util.Util;
 import crafttweaker.annotations.ZenRegister;
 import crafttweaker.api.data.IData;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.FMLCommonHandler;
 import net.minecraftforge.fml.common.eventhandler.Event.Result;
+import net.minecraftforge.fml.relauncher.Side;
 import stanhebben.zenscript.annotations.ZenClass;
 import stanhebben.zenscript.annotations.ZenMethod;
 
@@ -17,14 +19,16 @@ public class CTHUD {
 
 	@ZenMethod
 	public static void setStatus(IData v) {
-		Result newStatus = Util.parseResult(v);
-		if (newStatus != status) {
-			if (newStatus == Result.DEFAULT) {
-				MinecraftForge.EVENT_BUS.unregister(ClientEventHandler.class);
-			} else {
-				MinecraftForge.EVENT_BUS.register(ClientEventHandler.class);
+		if (FMLCommonHandler.instance().getSide() == Side.CLIENT) {
+			Result newStatus = Util.parseResult(v);
+			if (newStatus != status) {
+				if (newStatus == Result.DEFAULT) {
+					MinecraftForge.EVENT_BUS.unregister(HUDEventHandler.class);
+				} else {
+					MinecraftForge.EVENT_BUS.register(HUDEventHandler.class);
+				}
+				status = newStatus;
 			}
-			status = newStatus;
 		}
 	}
 }
